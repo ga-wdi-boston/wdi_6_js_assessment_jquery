e6f7072683134ec58a25bfa92bdc941b61b342fc
zhbanton <zhbanton@gmail.com>
2014-07-18 14:27:50 -0400

    donezo

--- jquery.js
+++ jquery.js
@@ -3,49 +3,50 @@
 ///// Question 1
 // Make the "Highlight Quotes" link add the class "highlighted" to all quotes
 // within articles (the <q> elements).
+$(document).ready(function() {
+  $('#highlight-quotes').click(function(event) {
+    $('q').addClass('highlighted');
+    event.preventDefault();
+  });
 
-$('#highlight-quotes').click(function(event) {
-  $('q').addClass('highlighted');
-  event.preventDefault();
-});
+  ///// Question 2
+  // Make the "Toggle Articles" link hide or show the content, and only the
+  // content, of all articles.
 
-///// Question 2
-// Make the "Toggle Articles" link hide or show the content, and only the
-// content, of all articles.
+  $('#toggle-articles').click(function(event) {
+    $('article').find($('p')).toggle();
+    event.preventDefault();
+  });
 
-$('#toggle-articles').click(function(event) {
-  $('article').find($('p')).toggle();
-  event.preventDefault();
-});
+  ///// Question 3
+  // Make the "Close" link in the alert at the top of the page remove the alert
+  // from the page entirely (don't just hide it).
 
-///// Question 3
-// Make the "Close" link in the alert at the top of the page remove the alert
-// from the page entirely (don't just hide it).
+  $('.dismiss-alert').click(function(event) {
+    $('.alert').remove();
+    event.preventDefault();
+  });
 
-$('.dismiss-alert').click(function(event) {
-  $('.alert').remove();
-  event.preventDefault();
-});
+  ///// Question 4
+  // Make the comment form functional: Typing something into the input and
+  // pressing Enter or clicking the Comment button should add a new list item to
+  // the list of comments, containing that text.
 
-///// Question 4
-// Make the comment form functional: Typing something into the input and
-// pressing Enter or clicking the Comment button should add a new list item to
-// the list of comments, containing that text.
+  $('form').submit(function(event) {
+    var comment = $('<li>');
+    comment.text($('.newCommentText').val());
+    $('.comments').append(comment);
+    $('.newCommentText').val('');
 
-$('form').submit(function(event) {
-  var comment = $('<li>');
-  comment.text($('.newCommentText').val());
-  $('.comments').append(comment);
-  $('.newCommentText').val('');
+    event.preventDefault();
+  });
 
-  event.preventDefault();
-});
+  ///// Question 5
+  // Make it so clicking on a comment list item toggles the class "favorite" on
+  // it, including comments that were added after the page loaded.
 
-///// Question 5
-// Make it so clicking on a comment list item toggles the class "favorite" on
-// it, including comments that were added after the page loaded.
-
-$('.comments').on('click', 'li', function(event) {
-  $(this).toggleClass('favorite');
-  event.preventDefault();
+  $('.comments').on('click', 'li', function(event) {
+    $(this).toggleClass('favorite');
+    event.preventDefault();
+  });
 });
