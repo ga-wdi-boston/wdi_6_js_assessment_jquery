3aae16439b4a261d5272d8f57f107d8ba3f47b63
Emma Gardner <emmagardner@outlook.com>
2014-07-18 14:27:01 -0400

    second round

--- jquery.js
+++ jquery.js
@@ -6,7 +6,7 @@
 
 $(document).ready(function(){
   $('#highlight-quotes').click(function(){
-    $('#aricles q').attr('class', 'highlighted');
+    $('#aricles, q').attr('class', 'highlighted');
   });
 
 
@@ -21,7 +21,7 @@ $(document).ready(function(){
 ///// Question 3
 // Make the "Close" link in the alert at the top of the page remove the alert
 // from the page entirely (don't just hide it).
-  $('.dissmiss-alert').click(function(){
+  $('.alert, .dissmiss-alert').click(function(){
     $('.alert').remove();
   });
 
@@ -29,12 +29,14 @@ $(document).ready(function(){
 // Make the comment form functional: Typing something into the input and
 // pressing Enter or clicking the Comment button should add a new list item to
 // the list of comments, containing that text.
-  $('.new-comment').submit(function(){
-    var newComment = $('.new-comment, .new_comment_text').val();
-    $('<li>').text(newComment).append('comments');
+  $('.new-comment').submit(function(event){
+    var newComment = $('.new-comment .new-comment-text').val();
+    $('<li>').text(newComment).append('.comments');
+    event.preventDefault();
   });
 
 ///// Question 5
 // Make it so clicking on a comment list item toggles the class "favorite" on
 // it, including comments that were added after the page loaded.
+
 });
